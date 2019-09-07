e9e7b78be6ee9431328d6c0a2f508e0067138a0e
Abhinav Anand <anand_abhinav@apple.com>
2019-09-07 06:49:05 +0530

    Static UI committed.

---
+++ github-profile-service/src/main/resources/static/precache-manifest.483029a9c24618ac377231fdbdcb5708.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60c17ab66e628d652ea7e77a2885907b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16fba7a480b6d9d889a2",
+    "url": "/static/css/main.ce417ad9.chunk.css"
+  },
+  {
+    "revision": "e1bd0e594ee122178062",
+    "url": "/static/js/2.c003b023.chunk.js"
+  },
+  {
+    "revision": "16fba7a480b6d9d889a2",
+    "url": "/static/js/main.e5545da7.chunk.js"
+  },
+  {
+    "revision": "5f81b709d4be09543c58",
+    "url": "/static/js/runtime~main.0c7f1e8c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
