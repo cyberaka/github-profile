59b9ca4d362878a2b9d119d1802c136f24adfce5
Abhinav Anand <anand_abhinav@apple.com>
2019-09-07 05:38:52 +0530

    Inital import done.

---
+++ github-profile-service/src/main/java/com/cyberaka/kg/githubprofileservice/GithubController.java
@@ -0,0 +1,49 @@
+package com.cyberaka.kg.githubprofileservice;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@RestController
+public class GithubController {
+
+    @RequestMapping("/profile")
+    public String loadProfile(@RequestParam(value="id", defaultValue="cyberaka") String id) throws IOException {
+        String url = "https://api.github.com/users/" + id;
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        //add request header
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        String resultString = response.toString();
+        System.out.println(resultString);
+
+        return resultString;
+    }
+}
---
+++ github-profile-service/src/main/java/com/cyberaka/kg/githubprofileservice/GithubProfileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cyberaka.kg.githubprofileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubProfileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubProfileServiceApplication.class, args);
+	}
+
+}
